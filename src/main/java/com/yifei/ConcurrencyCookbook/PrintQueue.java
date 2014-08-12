cbab1fd31c7f889aab21f9452cae7e856042dc6d
evenX86 <evenX86@gmail.com>
2014-08-12 21:12:20 +0800

    [add] 增加线程资源多副本的并发访问控制

--- src/main/java/com/yifei/ConcurrencyCookbook/PrintQueue.java
+++ src/main/java/com/yifei/ConcurrencyCookbook/PrintQueue.java
@@ -1,26 +1,55 @@
 package com.yifei.ConcurrencyCookbook;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by xuyifei01 on 14-8-12.
  */
 public class PrintQueue {
     private final Semaphore semaphore;
+    private boolean freePrinters[];
+    private Lock lockPrinters;
 
     public PrintQueue() {
-        this.semaphore = new Semaphore(2);
+        freePrinters = new boolean[3];
+        this.semaphore = new Semaphore(3);
+        for (int i = 0; i < 3; i++) {
+            freePrinters[i] = true;
+        }
+        lockPrinters = new ReentrantLock();
     }
 
+    private int getPrinter() {
+        int ret = -1;
+        try {
+            lockPrinters.lock();
+            for (int i = 0; i < freePrinters.length; i++) {
+                if (freePrinters[i]) {
+                    ret = i;
+                    freePrinters[i] = false;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lockPrinters.unlock();
+        }
+        return ret;
+    }
 
     public void printJob(Object document) {
         try {
             //通过调用acquire来获得信号量，必须通过这个方法来获得信号量
             semaphore.acquire();    //Acquires a permit, if one is available and returns immediately
+            int assignedPrinter = getPrinter();
             long duration = (long) (Math.random() * 10);
-            System.out.printf("%s: PrintQueue:Printing a Job during %d seconds\n",
-                    Thread.currentThread().getName(), duration);
+            System.out.printf("%s: PrintQueue:Printing a Job in Printer %d during %d seconds\n",
+                    Thread.currentThread().getName(), assignedPrinter,duration);
             Thread.sleep(duration);
+            freePrinters[assignedPrinter]=true;
 
         } catch (InterruptedException e) {
             e.printStackTrace();
