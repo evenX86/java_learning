7a783c6d7cbeadd83c30d549d518ef4fcb27f287
evenX86 <evenX86@gmail.com>
2014-09-02 23:26:03 +0800

    [add]使用原子变量

--- src/main/java/com/yifei/ConcurrencyCookbook/collection/Bank.java
+++ src/main/java/com/yifei/ConcurrencyCookbook/collection/Bank.java
@@ -3,17 +3,21 @@ package com.yifei.ConcurrencyCookbook.collection;
 /**
  * Created by xuyifei on 14-9-2.
  */
-public class Bank implements Runnable{
+public class Bank implements Runnable {
     public Bank(Account account) {
         this.account = account;
     }
 
     private Account account;
+
     @Override
     public void run() {
-        account.subtracAmount(1000);
+        for (int i = 0; i < 10; i++) {
+            account.subtracAmount(1000);
+        }
 
     }
+
     public static void main(String[] args) {
         Account account1 = new Account();
         account1.setBalance(1000);
