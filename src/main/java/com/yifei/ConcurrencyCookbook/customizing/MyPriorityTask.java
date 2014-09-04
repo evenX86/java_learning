9ab89b6eb20a4afe684116133ce3e0d26fe442c4
evenX86 <evenX86@gmail.com>
2014-09-04 11:41:13 +0800

    [add] 添加执行函数

--- src/main/java/com/yifei/ConcurrencyCookbook/customizing/MyPriorityTask.java
+++ src/main/java/com/yifei/ConcurrencyCookbook/customizing/MyPriorityTask.java
@@ -1,5 +1,7 @@
 package com.yifei.ConcurrencyCookbook.customizing;
 
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -38,6 +40,39 @@ public class MyPriorityTask implements Runnable, Comparable<MyPriorityTask> {
             return -1;
         }
         return 0;
+    }
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(4, 4, 1, TimeUnit.SECONDS, new PriorityBlockingQueue<Runnable>());
+        for (int i = 0; i < 4; i++) {
+
+            try {
+                Thread.sleep(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            MyPriorityTask task = new MyPriorityTask(i, "Task" + i);
+            executor.execute(task);
+
+        }
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 4; i < 8; i++) {
+            MyPriorityTask task = new MyPriorityTask(i, "Task " + i);
+            executor.execute(task);
+        }
+        executor.shutdown();
+        //之前使用了executor.shutdownNow（）结果一直报线程interrrupt的exception
+        //shutdown() 方法在终止前允许执行以前提交的任务，而 shutdownNow() 方法阻止等待任务启动并试图停止当前正在执行的任务。
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("End Of The Program");
     }
 
 }
